4a897dfcbbcf3da1b819a0e9b6ca7ff9ebc742ba
Jakub Ryszka <ryszkajakub6@gmail.com>
2023-02-07 00:01:40 +0100

    add first login form

--- client/src/pages/SignIn/SignIn.tsx
+++ client/src/pages/SignIn/SignIn.tsx
@@ -1,6 +1,41 @@
-import * as React from "react";
+import React, { useState } from "react";
 import "./SignIn.css";
 
 export const SignIn = () => {
-  return <div className="main">Logowanie</div>;
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  const handlePasswordChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setPassword(event.target.value);
+  };
+
+  const handleEmailCHange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setEmail(event.target.value);
+  };
+
+  const handleLogIn = () => {
+    
+  }
+  return (
+    <div className="main">
+      <section className="loginContainer">
+        <h2>Welcome</h2>
+        <section id="form">
+          <label>
+            email:{" "}
+            <input type="email" value={email} onChange={handleEmailCHange} />
+          </label>
+          <label>
+            password:{" "}
+            <input
+              type="password"
+              value={password}
+              onChange={handlePasswordChange}
+            />
+          </label>
+          <button onClick={handleLogIn}>Log In</button>
+        </section>
+      </section>
+    </div>
+  );
 };
