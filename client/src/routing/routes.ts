f24e4e80063bda42dc25c05894cacdeaf47f5933
Jakub Ryszka <ryszkajakub6@gmail.com>
2023-02-08 17:11:22 +0100

    add css to home

--- client/src/routing/routes.ts
+++
@@ -1,9 +0,0 @@
-import { SignIn } from "../pages";
-
-interface RouteI {
-  name: string;
-  path: string;
-  component: any;
-}
-
-export const routes = [{ name: "SignIn", path: "/", component: SignIn }];
