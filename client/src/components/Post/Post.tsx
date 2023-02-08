6a4d7410c59b461219426e9d6cecc57098842241
Jakub Ryszka <ryszkajakub6@gmail.com>
2023-02-08 19:14:33 +0100

    add logic to Post

--- client/src/components/Post/Post.tsx
+++ client/src/components/Post/Post.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import { PostProps } from "./types";
 
 export const Post: React.FC<PostProps> = ({
@@ -7,5 +7,22 @@ export const Post: React.FC<PostProps> = ({
   reactions,
   comments,
 }) => {
-  return <></>;
+  const [isCommentFormVisible, setIsCommentFormVisible] = useState(false);
+
+  const handleVisibleChange = () =>
+    setIsCommentFormVisible((prevState) => !prevState);
+
+  const commentText = isCommentFormVisible ? "Add Comment" : "Cancel";
+  return (
+    <section>
+      <div className="header">{user.fullName}</div>
+      // Slider
+      // Content Place
+      <div className="behaviors">
+        <div>Like It</div>
+        <div onClick={handleVisibleChange}>{commentText}</div>
+      </div>
+      //CommentForm
+    </section>
+  );
 };
