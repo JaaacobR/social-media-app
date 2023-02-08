addef6fab6dc957acd35d7ac76385b090db46fea
Jakub Ryszka <ryszkajakub6@gmail.com>
2023-02-08 11:48:16 +0100

    add home page

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,13 +1,14 @@
 import React, { useEffect } from "react";
 import "./App.css";
 import { Routes, Route } from "react-router";
-import { SignIn } from "./pages";
+import { SignIn, Home } from "./pages";
 
 function App() {
   return (
     <>
       <Routes>
         <Route path="/" element={<SignIn />} />
+        <Route path="/Home" element={<Home />} />
       </Routes>
     </>
   );
